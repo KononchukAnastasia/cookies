4dbd9735849a36562cf35d796aa1cf382ef09135
AnastasiaKononchuk <nastyakononchuk@me.com>
2024-09-30 14:59:01 +0700

    add Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppComponent } from './app.component';
+// app.module.ts
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppComponent } from "./app.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http"; // Проверьте путь к компоненту
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent // Добавьте ваш компонент сюда
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
